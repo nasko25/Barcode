ea1184091ecd9c1a916beb92e9b4ba7985873614
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-10-19 21:50:49 +0300

    Initial commit.

---
+++ app/src/main/java/com/example/atanaspashov/barcode/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.atanaspashov.barcode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class MainActivity extends AppCompatActivity {
+    TextView barcodeResult;
+    public String buttonName = "button";
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        tv.setText(tv.getText() + " and from Java :O");
+        //https://stackoverflow.com/questions/44842887/how-to-access-a-java-variable-in-strings-xml
+
+
+
+        barcodeResult = (TextView)findViewById(R.id.sample_text);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // add click event to the scan barcode button
+    public void scanBarcode(View v){
+        Intent intent = new Intent(this, ScanBarcodeActivity.class);
+        startActivityForResult(intent, 0);
+
+    }
+    // override onActivityResult to get barcode from ScanBarcodeActivity
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode==0){
+            if (resultCode==CommonStatusCodes.SUCCESS){
+                if(data!=null){
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    barcodeResult.setText("Barcode value: " + barcode.displayValue);
+                }
+                else {
+                    barcodeResult.setText("No Barcpde Found");
+                }
+            }
+
+        }
+            else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/atanaspashov/barcode/ScanBarcodeActivity.java
@@ -0,0 +1,85 @@
+package com.example.atanaspashov.barcode;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class ScanBarcodeActivity extends Activity {
+    SurfaceView camera_preview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_barcode);
+
+        camera_preview = (SurfaceView) findViewById(R.id.camera_preview);
+        createCameraSource();
+    }
+
+    private void createCameraSource() {
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
+        final CameraSource cameraSource = new CameraSource.Builder(this, barcodeDetector).setAutoFocusEnabled(true).setRequestedPreviewSize(1600, 1024).build();
+        camera_preview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(ScanBarcodeActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                try {
+                    cameraSource.start(camera_preview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+            barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<Barcode> detections) {
+                    final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                    if(barcodes.size() > 0) {
+                        // send it to MainActivity
+                        Intent intent = new Intent();
+                        intent.putExtra("barcode", barcodes.valueAt(0));  // get latest barcode from the array
+                        setResult(CommonStatusCodes.SUCCESS, intent);
+                        finish();
+                    }
+                }
+            });
+    }
+}
