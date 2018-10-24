b54ca9da41a73c2966f794d35ba2773369edf65c
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-10-24 19:57:30 +0300

    Created a new class that holds the needed DB strings.

---
+++ app/src/main/java/com/example/atanaspashov/barcode/DB_Strings.java
@@ -0,0 +1,10 @@
+package com.example.atanaspashov.barcode;
+
+public class DB_Strings {
+
+    static final String database_URL = "localhost:3306";
+    static final String database_NAME = "Barcodes";
+    static final String username = "app_user";
+    static final String password = "pass";
+
+}
