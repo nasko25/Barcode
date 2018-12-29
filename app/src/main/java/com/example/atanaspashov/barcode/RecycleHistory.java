3a97770ded8612af64ac89fed6cffd8611c39baf
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-12-30 00:32:29 +0200

    Added a new activity that will display the contents of the Recycle History database.

---
+++ app/src/main/java/com/example/atanaspashov/barcode/RecycleHistory.java
@@ -0,0 +1,13 @@
+package com.example.atanaspashov.barcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecycleHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_history);
+    }
+}
