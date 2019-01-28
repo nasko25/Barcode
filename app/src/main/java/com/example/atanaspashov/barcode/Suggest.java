2fec08412e135d5f3082b8ce15b6e451d84a32f1
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2019-01-28 22:14:00 +0200

    Implemented a TCP socket to send the "Suggest" information, that the user will input, to a server.

--- app/src/main/java/com/example/atanaspashov/barcode/Suggest.java
+++ app/src/main/java/com/example/atanaspashov/barcode/Suggest.java
@@ -9,5 +9,6 @@ public class Suggest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggest);
+         new SendASuggestion().execute("");
     }
 }
